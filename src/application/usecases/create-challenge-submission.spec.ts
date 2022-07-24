14b55ef41fea6f8c751eff233f7fe68352165b33
zBiggui <bigguisenna@gmail.com>
2022-07-23 22:45:12 -0300

    feat: add create challenges submittion logic

--- src/application/usecases/create-challenge-submission.spec.ts
+++ src/application/usecases/create-challenge-submission.spec.ts
@@ -1,12 +1,35 @@
+import {InMemoryChallengesRepository} from "../../../tests/repositories/in-memory-challenges-repository";
+import {InMemoryStudentsRepository} from "../../../tests/repositories/in-memory-students-repository";
+import {Challenge} from "../../domain/entities/challenge";
+import {Student} from "../../domain/entities/student";
 import {CreateChallengeSubmission} from "./create-challenge-submission";
 
 describe('Create challenge submission use case', () => {
     it('should be able to create a new challenge submission', async () => {
-        const sut = new CreateChallengeSubmission();
+        const studentsRepository = new InMemoryStudentsRepository;
+        const challengesRepository = new InMemoryChallengesRepository();
+
+        const student = Student.create({
+            name: 'Diego',
+            email: 'doe@example.com',
+        });
+
+        const challenge = Challenge.create({
+            title: 'challenge 01',
+            instructionUrl: 'http//example.com'
+        })
+
+        studentsRepository.items.push(student)
+        challengesRepository.items.push(challenge)
+
+        const sut = new CreateChallengeSubmission(
+            studentsRepository,
+            challengesRepository,
+        );
 
         const response = await sut.execute({
-            studentId: 'fake-student-id',
-            challengeId: 'fake-challenge-id'
+            studentId: student.id,
+            challengeId: challenge.id
         });
 
         expect(response).toBeTruthy();
