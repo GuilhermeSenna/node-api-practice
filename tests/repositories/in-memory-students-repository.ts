2af40cf003feaa47d5fc3f38408c7a0d525d9138
zBiggui <bigguisenna@gmail.com>
2022-07-23 22:44:13 -0300

    feat: add database in memory

---
+++ tests/repositories/in-memory-students-repository.ts
@@ -0,0 +1,17 @@
+import {StudentsRepository} from "../../src/application/repositories/StudentsRepository";
+import {Student} from "../../src/domain/entities/student";
+
+export class InMemoryStudentsRepository implements StudentsRepository {
+
+    public items: Student[] = [];
+
+    async findById(id: string): Promise<Student | null> {
+        const student = this.items.find(student => student.id === id);
+
+        if (!student) {
+            return null;
+        }
+
+        return student;
+    }
+}
