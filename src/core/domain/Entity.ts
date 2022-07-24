549d4fbba7738c328e4a7a478d90ae823303853d
zBiggui <bigguisenna@gmail.com>
2022-07-23 22:43:51 -0300

    feat: add getter to Entity

--- src/core/domain/Entity.ts
+++ src/core/domain/Entity.ts
@@ -5,6 +5,10 @@ export abstract class Entity<T> {
     protected _id: string;
     public props: T;
 
+    get id() {
+        return this._id
+    };
+
     constructor(props: T, id?: string) {
         this.props = props;
 
